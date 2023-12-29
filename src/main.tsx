d3ab76cf546d5f45b6e0f89369ebd3ffd13f8dd0
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2023-12-29 10:11:04 -0700

    Chore: Install Unocss

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { Theme, ThemePanel } from '@radix-ui/themes';
 import App from './App.tsx'
 import '@radix-ui/themes/styles.css';
 import './index.css'
+import 'virtual:uno.css'
+
 
 
 
@@ -11,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Theme accentColor="indigo" grayColor="sand" radius="large" scaling="100%">
       <App />
-      <ThemePanel />
     </Theme>
   </React.StrictMode>,
 )
