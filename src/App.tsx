671c9588d3b5495a34b97827758a100f767a82d6
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2023-12-29 11:33:16 -0700

    UI: Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{
+      base: `
+      "navbar navbar navbar navbar"
+      "content content content content"
+      `,
+      lg: `
+      "navbar navbar navbar navbar"
+      "sidebar content content content"
+      `
+
+    }}>
+      <GridItem area='navbar' bg='coral'>
+        Navbar
+      </GridItem>
+      <Show above={'lg'}>
+        <GridItem area='sidebar' bg='dodgerblue'>
+          Sidebar
+        </GridItem>
+      </Show>
+      <GridItem area='content' bg='yellow'>
+        Content
+      </GridItem>
+    </Grid>
   )
 }
 
