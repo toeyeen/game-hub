983aedd5228f73865f8351a6bd8c525365a931df
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2023-12-29 10:02:57 -0700

    Install Radix UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import App from './App.tsx'
+import '@radix-ui/themes/styles.css';
 import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme accentColor="indigo" grayColor="sand" radius="large" scaling="100%">
+      <App />
+      <ThemePanel />
+    </Theme>
   </React.StrictMode>,
 )
