bca208688ae0e058fff8e601e447b4d0e92cbe52
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2023-12-30 11:34:53 -0700

    Build the color mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import gamifyLogo from '../assets/gamify.svg'
+import ColorModeSwitch from './ColorModeSwitch'
 
 function Navbar() {
   return (
-    <HStack>
-      <Image src={gamifyLogo} alt='logo' boxSize='60px' marginLeft={2} />
-      <Text color='white'>
-        Navbar </Text>
+    <HStack justifyContent={'space-between'} paddingX={3} >
+      <Image src={gamifyLogo} alt='logo' boxSize='60px' />
+      <ColorModeSwitch />
     </HStack>
   )
 }
