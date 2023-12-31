dc4a92d126309c7a706c720f456acb3b88bd00ae
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2023-12-31 15:24:01 -0700

    UI: Refactor Game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,8 +7,6 @@ import { GameQuery } from '../App'
 
 interface GameGridProps {
   gameQuery: GameQuery
-  // selectedGenre: Genre | null
-  // selectedPlatform: Platform | null
 }
 
 
@@ -16,19 +14,19 @@ function GameGrid({ gameQuery }: GameGridProps) {
 
   const { data: games, error, isLoading } = useGames({ gameQuery })
 
-  return (
-    <>
+  if (error) return <Text>{error}</Text>
 
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{
-        base: 1,
-        sm: 2,
-        md: 3,
-      }} spacing={10}>
-        {isLoading && Array(9).fill(0).map((_, index) => <GameCardSkeleton key={index} />)}
-        {games.map(game => <GameCard key={game.id} game={game} />)}
-      </SimpleGrid>
-    </>
+  return (
+
+    <SimpleGrid columns={{
+      base: 1,
+      sm: 2,
+      md: 3,
+    }} spacing={10}>
+      {isLoading && Array(9).fill(0).map((_, index) => <GameCardSkeleton key={index} />)}
+      {games.map(game => <GameCard key={game.id} game={game} />)}
+    </SimpleGrid>
+
   )
 }
 
